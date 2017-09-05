fd64e4ce063afb6c839445baffef514869dc881e
austinbavaro <Austin.Bavaro1@marist.edu>
2017-09-05 14:56:07 -0400

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,14 @@
+noun = input("Enter a noun: ")
+
+verb = input("Enter a verb: ")
+
+adjective = input("Enter an adjective: ")
+
+place = input("Enter a place: ")
+             
+
+print("Look at that gigantic " + noun + "!")
+print("It is going to " + verb + " down the street!")
+print("That " + noun + " is extremely " + adjective + "!")
+print("I can't wait until we are at the " + place + " and away from this " + noun +".")
+
