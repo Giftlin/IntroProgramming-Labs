154bfc909e8ce635b5281a5dbeae3b6823503ba1
Giftlin <31629119+Giftlin@users.noreply.github.com>
2017-09-12 23:56:52 +0530

    Create add.py
    addition of 2 numbers

---
+++ add.py
@@ -0,0 +1,12 @@
+# Introduction to Programming
+# Author: Giftlin Rajaiah
+# Date: 9/12/2017
+def add(x,y):
+  result = 0
+  try:
+    result = x+y
+  except:
+    # handle all other exceptions
+    pass
+  return(result)
+# add(5,6)
